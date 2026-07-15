1d0c507ee23a00cc8ebfeff0ff472c7903360490
karoll jesus <karokamartinez@gmail.com>
2026-07-14 18:48:50 -0600

    implementing header and footer

---
+++ js/includes.js
@@ -0,0 +1,14 @@
+//Crag un archivo HTML externo y lo inserta en un elemento de la pagina
+
+//esto se hace para agarrar el header y fooer y solamente incorporarlo en cada página evitando duplicar código
+
+async function cargarComponente(nombreArchivo, idDestino) {
+    //fetch pide al archivo header.html y cuando llega lo mete dentro de un div que sirve como placeholder "marcador de posicion"
+
+    const respuesta = await fetch('partials/$nombreArchivo');
+    const html = await respuesta.text();
+    document.getElementById(idDestino).innerHTML = html;
+}
+
+cargarComponente('header.html', 'header-placeholder');
+cargarComponente('footer.html', 'footer-placeholder');
